dd276c124805b4d14c17783f7d4a4fe20b83a444
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-04-30 18:31:02 +0200

    add "this.repos" to the User class

--- app/user.js
+++ app/user.js
@@ -3,12 +3,13 @@ export default class User {
     this.name = "Luis Alejandro";
     this.lastName = "Montilla";
     this.username = "AlejoVE";
+    this.repos = [];
   }
 
   getRepos() {
     fetch(`https://api.github.com/users/AlejoVE/repos`)
       .then((resp) => resp.json())
-      .then((data) => console.log(data))
+      .then((data) => this.repos.push(data))
       .catch((error) => console.log("Error", error));
   }
 }
